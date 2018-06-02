9d00e8bb68dae6fd7f00b1b3dd2009f959c57ed6
MengqiQ <303514197@qq.com>
2018-06-02 20:56:35 +0800

    try

---
+++ CountBinarySubstrings696.java
@@ -0,0 +1,18 @@
+public class CountBinarySubstrings696 {
+    public int countBinarySubstrings(String s) {
+        if (s == null) {
+            return 0;
+        }
+        int count = 0;
+    }
+    private int count(String s, int count, int start, int nums) {
+        if (count == 0) {
+            return nums;
+        }
+        int count = 0;
+        while (s.charAt(start) == 0) {
+            count ++;
+        }
+        while ()
+    }
+}
